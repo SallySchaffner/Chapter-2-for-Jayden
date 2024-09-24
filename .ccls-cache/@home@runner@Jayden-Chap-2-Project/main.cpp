52034939228a8e53842e670713b5cd758263904e
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-09-24 16:07:18 +0000

    Code for Energy Drinks program

--- .ccls-cache/@home@runner@Jayden-Chap-2-Project/main.cpp
+++ .ccls-cache/@home@runner@Jayden-Chap-2-Project/main.cpp
@@ -1,4 +1,16 @@
-// Main branch for Chapt 2 Programs (3 programs in this repository)
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Chapter 2 Programs\n"; }
+int main() {
+  // Calculate the number of customers purchasing energy drinks
+  int energyDrinkCustomers = 0.15 * 16500;
+  cout << "Approximate number of customers purchasing energy drinks: "
+       << energyDrinkCustomers << endl;
+
+  // Calculate the number of customers preferring citrus-flavored energy drinks
+  int citrusDrinkCustomers = 0.58 * 16500;
+  cout << "Approximate number of customers preferring citrus-flavored energy "
+       << "drinks: " << citrusDrinkCustomers << endl;
+
+  return 0;
+}
